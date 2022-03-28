85315a1c092b19050d3c11120b8e492114e1c85a
Menna A. Awadallah <mennashaheen2357@gmail.com>
2022-03-28 15:31:30 +0200

    Create ex02.sql

---
+++ week05/ex02.sql
@@ -0,0 +1,30 @@
+create table CGroup(
+  groupId INTEGER PRIMARY KEY
+);
+create table Company(
+  companyId INTEGER PRIMARY KEY
+);
+create table Item(
+   itemId INTEGER PRIMARY KEY
+);
+create table Plant(
+  plantId INTEGER PRIMARY KEY
+);
+create table production(
+  itemId INTEGER,
+  plantId INTEGER,
+  foreign key(itemId) references Item(itemId),
+  foreign key(plantId) references Plant(plantId)
+);
+create table companyPlants(
+  companyId INTEGER,
+  plantId INTEGER,
+  foreign key(companyId) references Company(companyId),
+  foreign key(plantId) references Plant(plantId)
+ );
+ create table groupCompanies(
+   companyId INTEGER,
+   groupId INTEGER,
+   foreign key(companyId) references Company(companyId),
+   foreign key(groupId) references CGroup(groupId)
+ );
