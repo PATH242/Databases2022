441912efa2215187b5772776be2fc89cf525c1c0
Mennatullah Awadallah <52926455+PATH242@users.noreply.github.com>
2022-05-02 02:22:17 +0300

    Create ex02.sql

---
+++ extra assignment- 2/ex02.sql
@@ -0,0 +1,18 @@
+EXPLAIN ANALYZE
+SELECT *
+FROM purchases_products_list 
+JOIN products ON 
+products.id = purchases_products_list.product_id
+JOIN sales ON 
+products.type = sales.type;
+
+CREATE INDEX idx_products ON products USING HASH (type);
+CREATE INDEX idx_sales ON sales USING hash (type);
+
+EXPLAIN ANALYZE
+SELECT *
+FROM purchases_products_list
+JOIN products ON
+products.id = purchases_products_list.product_id
+JOIN sales ON 
+products.type = sales.type;
