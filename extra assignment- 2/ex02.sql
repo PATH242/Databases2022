aedec275411c182279049fd8f0d2230e729fe499
Mennatullah Awadallah <52926455+PATH242@users.noreply.github.com>
2022-05-02 02:22:48 +0300

    Update ex02.sql

--- extra assignment- 2/ex02.sql
+++ extra assignment- 2/ex02.sql
@@ -6,8 +6,8 @@ products.id = purchases_products_list.product_id
 JOIN sales ON 
 products.type = sales.type;
 
-CREATE INDEX idx_products ON products USING HASH (type);
-CREATE INDEX idx_sales ON sales USING hash (type);
+CREATE INDEX idx_products ON products USING HASH (type DESC);
+CREATE INDEX idx_sales ON sales USING hash (type DESC);
 
 EXPLAIN ANALYZE
 SELECT *
